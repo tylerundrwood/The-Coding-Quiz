7cb7de5064d57612837ed4afe8ca0c2db00a70e9
Tyler Underwood <twood9600@yahoo.com>
2023-05-07 14:46:50 -0400

    developed html structure and js variables

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+let start = document.getElementById('start');
+let questions = document.getElementById('questions');
+let results = document.getElementById('results');
+let answers = document.getElementById('answers');
+let resultMessage = document.getElementById('result-message');
+let scoreboard = document.getElementById('scoreboard');
+let highscore = document.getElementById('highscore');
+let inputElement = document.getElementById('initials');
+let submitButton = document.getElementById('submit-button');
+let clearButton = document.getElementById('clear-button');
+
+
