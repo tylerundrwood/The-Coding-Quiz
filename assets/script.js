a63372b31e967868aad383f8a540cc21989effd8
Tyler Underwood <twood9600@yahoo.com>
2023-05-09 21:19:54 -0400

    added timer and began scoreboard

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ let answers = document.getElementById('answers');
 let resultMessage = document.getElementById('result-message');
 let scoreboard = document.getElementById('scoreboard');
 let topScore = document.getElementById('topScore');
+let finalAnswerElement = document.getElementById('finalAnswer')
 let inputElement = document.getElementById('initials');
 let submitButton = document.getElementById('submit-button');
 let clearButton = document.getElementById('clear-button');
@@ -81,4 +82,115 @@ function startTest(){
     //remove the hidden attribute
     hideQuiz();
     questions.removeAttribute('hidden');
+     //start question with an index of 0
+     currentQuestion = 0;
+     displayQuestion();
+
+     //when the start button is pressed, the timer is set to 60 seconds
+    time = 60;
+    // time goes down by 1 second
+    interval = setInterval(countdown,1000);
+    
+    //initiate displayCount function. when start is clicked countdown will appear in top right of page.
+    displayCount();
 }
+//function for when time is at 0, end the quiz
+function countdown(){
+    time--;
+    displayCount();
+
+    if(time < 1){
+        endTest();
+    }
+}
+
+//variable to access display time/displaying the time 
+function displayCount() {
+    displayTime.textContent = time;
+}
+//reveal the question and the question option
+function displayQuestion(){
+    let questions = question[currentQuestion];
+    let options = questions.options;
+
+    let questionElement = document.querySelector('#question-choice');
+    questionElement.textContent = question.questionChoice;
+
+    for(let x = 0; x < options.length; x++) { 
+        let option = options[x];
+        let optionButton = document.querySelector("#option" + x);
+        optionButton.textContent = option;
+    }
+}
+
+//compare the users choice with the answer
+function choiceIsCorrect(optionButton) {
+    return optionButton.textContent === questions[currentQuestion].answer;
+}
+
+//if answer is incorrect subtract time
+function checkAnswer(eventObject) {
+    let optionButton = eventObject.target;
+    resultDiv.style.display = "block";
+    if (choiceIsCorrect(optionButton)) {
+        resultText.textContent = "Correct!";
+        setTimeout(hideResultText, 1000);
+    } else {
+        resultText.textContent = "Incorrect!";
+        setTimeout(hideResultText, 1000);
+        if (time >= 10) {
+            time = time - 10;
+            displayCount();
+        } else {
+            time = 0;
+            displayCount();
+            endTest();
+        }
+    }
+    //after the question is answered then switches to the next question
+    currentQuestion++;
+    //when every question is answered, the quiz ends
+    if (currentQuestion < question.length) {
+        displayQuestion();
+    } else{
+        endTest();
+    }
+}
+
+//when the test has ended, show score and clear the timer
+function endTest(){
+    clearInterval(interval);
+    hideQuiz();
+    answers.removeAttribute('hidden');
+    finalAnswer.textContent = time;
+}
+
+function saveScore(event) {
+    event.preventDefault();
+
+    //only written if initials are valid
+    if (!inputElement.value) {
+        alert('Enter your initials. Then press submit!');
+        return;
+    }
+    //save score and initials
+    let scoreboardItem = {
+        initials: inputElement.value,
+        finalAnswer: time,
+
+    };
+    updateSavedScoreboard(scoreboardItem)
+    //hide all except for the scoreboard
+    hideQuiz();
+    scoreboard.removeAttribute('hidden');
+    displayScoreboard();
+}
+
+//update local storage with the scoreboard
+function updateSavedScoreboard(scoreboardItem) {
+    let topScore = getScoreboard();
+    topScore.push(scoreboardItem);
+    localStorage.setItem('topScore', JSON.stringify(topScore));
+}
+
+
